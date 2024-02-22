77c4ae2b29bd63bd4461b33e136fe8a3e5b7a6cd
Allen <alau002@citymail.cuny.edu>
2024-02-22 17:11:53 -0500

    ts

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,29 +24,29 @@ cd /opt/sentiment_analysis_pipeline
 git config pull.rebase false
 git pull
 
-#azure account login 
-az login
+# #azure account login 
+# az login
 
-#get Prefect cloud API key from Azure Key Vault 
-export PREFECT__CLOUD__API_KEY=$(az keyvault secret show --vault-name prefectsentimentanalysis --name prefect-key --query value -o tsv)
+# #get Prefect cloud API key from Azure Key Vault 
+# export PREFECT__CLOUD__API_KEY=$(az keyvault secret show --vault-name prefectsentimentanalysis --name prefect-key --query value -o tsv)
 
-#check if secrete retrieval was successful 
-if [ $? -ne 0 ]; then
-    echo "Error retrieving Prefect Cloud API key from Azure Key Vault"
-    exit 1
-fi
+# #check if secrete retrieval was successful 
+# if [ $? -ne 0 ]; then
+#     echo "Error retrieving Prefect Cloud API key from Azure Key Vault"
+#     exit 1
+# fi
 
-#log message for successful key retrieval 
-echo "Prefect Cloud API key retrieved successfully."
+# #log message for successful key retrieval 
+# echo "Prefect Cloud API key retrieved successfully."
 
-#login to prefect cloud
-prefect cloud login -k $PREFECT__CLOUD__API_KEY
+# #login to prefect cloud
+# prefect cloud login -k $PREFECT__CLOUD__API_KEY
 
-#run defined deployment 
-prefect deployment run webscrape-extract/webscrape-extract
+# #run defined deployment 
+# prefect deployment run webscrape-extract/webscrape-extract
 
-#start default agent 
-prefect agent start -q 'default' --run-once
+# #start default agent 
+# prefect agent start -q 'default' --run-once
 
-# Start an interactive shell at the end
+# # Start an interactive shell at the end
 exec /bin/bash
